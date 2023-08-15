06df5725abf495503c6e4603b195dac9db64fdb9
yuli-r <iuliana.rotaru234@gmail.com>
2023-08-15 17:22:34 +0100

    Add cards images

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,7 +10,8 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh',
+            'react-hooks'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
